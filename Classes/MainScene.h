9f404133721b1fe39f31c9225da3524b430725ee
859684581@qq.com <ziyousjw5211314>
2019-12-03 11:44:44 +0800

    完成背包界面

--- Classes/MainScene.h
+++ Classes/MainScene.h
@@ -11,14 +11,21 @@ public:
 
     virtual bool init();
     
+	void CreateLayer(cocos2d::Size visibleSize, cocos2d::Vec2 origin);
+
+	void CreateBagLayer(cocos2d::Size visibleSize, cocos2d::Vec2 origin,cocos2d::Layer* layer);
     // a selector callback
     void menuCloseCallback(cocos2d::Ref* pSender);
     
+	void onclickCloseCallBack(cocos2d::Ref* pSender,cocos2d::Layer* layer);
+
 	void intoLearnPageCallback(cocos2d::Ref* pSender);
 
 	void intoShopPageCallback(cocos2d::Ref* pSender);
 
 	void intoSettingsCallback(cocos2d::Ref* pSender);
+
+	void onclickBagCallback(cocos2d::Ref* pSender,cocos2d::Size size,cocos2d::Vec2 orign,cocos2d::Layer* layer);
     // implement the "static create()" method manually
     CREATE_FUNC(Main);
 };
